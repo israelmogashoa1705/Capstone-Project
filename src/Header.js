992e3b7e27b32f3fafb09dc51041fe074132c148
Mogau Mogashoa <israelmogashoa@gmail.com>
2026-05-25 11:13:26 +0200

    added meta tags, semantic structure, and components

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon Logo" />
+      <h1>Little Lemon</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main>
+      <h2>Welcome to Little Lemon</h2>
+      <p>Fresh Mediterranean food made with love.</p>
+    </main>
+  );
+}
+
+export default Main;
